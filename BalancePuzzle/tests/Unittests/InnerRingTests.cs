0bcec99bd30d7b3ca8266a80858c5c86485e9ba7
williambza <william.brander@gmail.com>
2024-02-07 09:02:24 +0200

    Add tests for inner ring

--- BalancePuzzle/tests/Unittests/InnerRingTests.cs
+++ BalancePuzzle/tests/Unittests/InnerRingTests.cs
@@ -5,54 +5,43 @@ namespace Unittests
         [Fact]
         public void NextLightInRingWithoutWrapping()
         {
-            var nextLight = Calculate.CalculateOffset(10, 33, 0, 1);
-            Assert.Equal(11, nextLight);
+            var nextLight = Calculate.CalculateOffset(65, -18, 60, 2);
+            Assert.Equal(63, nextLight);
         }
 
         [Fact]
         public void NextLightInRingThatWraps()
         {
-            var nextWrappedLight = Calculate.CalculateOffset(33, 33, 0, 1);
-            Assert.Equal(1, nextWrappedLight);
+            var nextWrappedLight = Calculate.CalculateOffset(60, -18, 60, 1);
+            Assert.Equal(77, nextWrappedLight);
         }
 
-        //[Fact]
-        //public void TenLightsAfterWithoutWrapping()
-        //{
-        //    var tenNextWrappedLight = CalculateOffset(35, 25, 33, 10);
-        //    Assert.Equal(45, tenNextWrappedLight);
-        //}
+        [Fact]
+        public void TenLightsAfterWithoutWrapping()
+        {
+            var tenNextWrappedLight = Calculate.CalculateOffset(75, -18, 60, 10);
+            Assert.Equal(65, tenNextWrappedLight);
+        }
 
-        //[Fact]
-        //public void TenLightsAfterWithWrapping()
-        //{
-        //    var tenNextWrappedLight = CalculateOffset(58, 25, 33, 10);
-        //    Assert.Equal(43, tenNextWrappedLight);
-        //}
+        [Fact]
+        public void TenLightsAfterWithWrapping()
+        {
+            var tenNextWrappedLight = Calculate.CalculateOffset(65, -18, 60, 10);
+            Assert.Equal(73, tenNextWrappedLight);
+        }
 
-        //[Fact]
-        //public void PreviousLightWithoutWrapping()
-        //{
-        //    var previousLight = CalculateOffset(35, 25, 33, -1);
-        //    Assert.Equal(34, previousLight);
-        //}
+        [Fact]
+        public void PreviousLightWithoutWrapping()
+        {
+            var previousLight = Calculate.CalculateOffset(65, -18, 60, -1);
+            Assert.Equal(66, previousLight);
+        }
 
-        //[Fact]
-        //public void PreviousLightWithWrapping()
-        //{
-        //    var previousWrappedLight = CalculateOffset(34, 25, 33, -1);
-        //    Assert.Equal(58, previousWrappedLight);
-        //}
-
-        //public int CalculateOffset(int currentLightNumber, int numberOfLightsInRing, int ringStartCount, int distanceOffCurrentLight)
-        //{
-        //    if (distanceOffCurrentLight < 0)
-        //    {
-        //        return currentLightNumber + distanceOffCurrentLight + (currentLightNumber + distanceOffCurrentLight <= ringStartCount ? numberOfLightsInRing : 0);
-        //    }
-
-        //    var o = currentLightNumber - numberOfLightsInRing + distanceOffCurrentLight;
-        //    return o % numberOfLightsInRing + numberOfLightsInRing;
-        //}
+        [Fact]
+        public void PreviousLightWithWrapping()
+        {
+            var previousWrappedLight = Calculate.CalculateOffset(78, -18, 60, -1);
+            Assert.Equal(61, previousWrappedLight);
+        }
     }
 }
---
+++ BalancePuzzle/tests/Unittests/OuterRingTests.cs
@@ -0,0 +1,47 @@
+namespace Unittests
+{
+    public class OuterRingTests
+    {
+        [Fact]
+        public void NextLightInRingWithoutWrapping()
+        {
+            var nextLight = Calculate.CalculateOffset(10, 33, 0, 1);
+            Assert.Equal(11, nextLight);
+        }
+
+        [Fact]
+        public void NextLightInRingThatWraps()
+        {
+            var nextWrappedLight = Calculate.CalculateOffset(33, 33, 0, 1);
+            Assert.Equal(1, nextWrappedLight);
+        }
+
+        [Fact]
+        public void TenLightsAfterWithoutWrapping()
+        {
+            var tenNextWrappedLight = Calculate.CalculateOffset(15, 33, 0, 10);
+            Assert.Equal(25, tenNextWrappedLight);
+        }
+
+        [Fact]
+        public void TenLightsAfterWithWrapping()
+        {
+            var tenNextWrappedLight = Calculate.CalculateOffset(33, 33, 0, 10);
+            Assert.Equal(10, tenNextWrappedLight);
+        }
+
+        [Fact]
+        public void PreviousLightWithoutWrapping()
+        {
+            var previousLight = Calculate.CalculateOffset(15, 33, 0, -1);
+            Assert.Equal(14, previousLight);
+        }
+
+        [Fact]
+        public void PreviousLightWithWrapping()
+        {
+            var previousWrappedLight = Calculate.CalculateOffset(1, 33, 0, -1);
+            Assert.Equal(33, previousWrappedLight);
+        }
+    }
+}
