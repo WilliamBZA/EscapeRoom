1ca5caf89e93e8f17b0b655350ff9ab23873bc9b
williambza <william.brander@gmail.com>
2024-03-28 19:46:36 +0200

    Get MQTT working

---
+++ ToneLock/MqttHelper/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet.Client;
+using MQTTnet;
+using System.Text.Json;
+using System.Text;
+
+public class Program
+{
+    static async Task SubscribeToEvents(IMqttClient mqttClient)
+    {
+        var mqttClientOptions = new MqttClientOptionsBuilder()
+            .WithTcpServer("broker", 1883)
+            .WithCleanStart(false)
+            .Build();
+
+        mqttClient.ApplicationMessageReceivedAsync += e =>
+        {
+            Console.WriteLine("Received application message.");
+            Console.WriteLine(System.Text.Encoding.Default.GetString(e.ApplicationMessage.PayloadSegment));
+
+            return Task.CompletedTask;
+        };
+
+        await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+        await mqttClient.SubscribeAsync("escaperoom/puzzles/*", MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce, CancellationToken.None);
+
+        Console.WriteLine("MQTT client subscribed to topic.");
+    }
+
+    static async Task Main(string[] args)
+    {
+        var mqttFactory = new MqttFactory();
+        var difficulty = 5;
+
+        using (var mqttClient = mqttFactory.CreateMqttClient())
+        {
+            await SubscribeToEvents(mqttClient);
+
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("\tPress H to increase difficulty");
+            Console.WriteLine("\tPress E to decrease difficulty");
+            Console.WriteLine("\tPress R to restart");
+            Console.WriteLine("\tPress ENTER to exit");
+
+            while (true)
+            {
+                var input = Console.ReadLine().ToLowerInvariant();
+                switch (input)
+                {
+                    case "":
+                        return;
+
+                    case "h":
+                        difficulty++;
+
+                        var harderMessage = new MqttApplicationMessageBuilder()
+                            .WithTopic("escaperoom/puzzles/changedifficulty")
+                            .WithPayload($"{difficulty}")
+                            .Build();
+
+                        await mqttClient.PublishAsync(harderMessage);
+                        Console.WriteLine($"Changed difficulty to {difficulty}");
+                        break;
+
+                    case "e":
+                        difficulty--;
+
+                        var easierMessage = new MqttApplicationMessageBuilder()
+                            .WithTopic("escaperoom/puzzles/changedifficulty")
+                            .WithPayload($"{difficulty}")
+                            .Build();
+
+                        await mqttClient.PublishAsync(easierMessage);
+                        Console.WriteLine($"Changed difficulty to {difficulty}");
+                        break;
+
+                    case "r":
+                        var restartMessage = new MqttApplicationMessageBuilder()
+                            .WithTopic("escaperoom/puzzles/startroom")
+                            .WithPayload("")
+                            .Build();
+
+                        await mqttClient.PublishAsync(restartMessage);
+                        Console.WriteLine("Restarted room");
+                        break;
+                }
+            }
+        }
+    }
+}
