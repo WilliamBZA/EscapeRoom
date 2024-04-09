bf77f97b2bbb0256b6b1016bb7bbd7c76654dbf3
WilliamBZA <william.brander@gmail.com>
2024-04-09 20:30:29 +0200

    Add direct receive pump

--- EscapeRoomManager/Program.cs
+++ EscapeRoomManager/Program.cs
@@ -7,16 +7,19 @@ namespace EscapeRoomManager
     {
         static async Task Main(string[] args)
         {
-            var endpointConfiguration = new EndpointConfiguration("EscapeToomHelper");
+            var endpointConfiguration = new EndpointConfiguration("EscapeRoomHelper");
             endpointConfiguration.UseTransport(new MqttTransport("broker"));
 
             endpointConfiguration.SendFailedMessagesTo("error");
             endpointConfiguration.EnableInstallers();
-            //endpointConfiguration.UsePersistence<LearningPersistence>();
+            endpointConfiguration.UsePersistence<LearningPersistence>();
             endpointConfiguration.UseSerialization<SystemJsonSerializer>();
             endpointConfiguration.Recoverability().Delayed(d => d.NumberOfRetries(0));
+            endpointConfiguration.Recoverability().Immediate(d => d.NumberOfRetries(3));
 
             var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+            await endpointInstance.SendLocal(new RunStarted { RunId = "123" });
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
---
+++ NserviceBus.Mqtt/MessageWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NserviceBus.Mqtt
+{
+    class MessageWrapper
+    {
+        public string? Id { get; set; }
+        public required Dictionary<string, string> Headers { get; set; }
+        public required string Body { get; set; }
+    }
+}
