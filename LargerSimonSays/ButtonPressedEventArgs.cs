25f9e548036c69161ade57a84bee7e2d83275522
williambza <william.brander@gmail.com>
2025-06-11 12:55:10 +0200

    Add rfid reader and simon says

---
+++ LargerSimonSays/ButtonPressedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace LargerSimonSays;
+
+using System;
+
+public class ButtonPressedEventArgs : EventArgs
+{
+    public int ButtonIndex { get; }
+
+    public ButtonPressedEventArgs(int buttonIndex)
+    {
+        ButtonIndex = buttonIndex;
+    }
+}
